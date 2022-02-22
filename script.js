af88691602d7c63208a39f66b5709231d643993b
tokyo <oktoniuszevanya@gmail.com>
2022-02-22 15:41:23 +0700

    Menambahkan Introduction, Trailer dan Story Page

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ let navbarLinks = document.getElementsByClassName('navbar-links')[0]
 hamburger.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+let iframe = document.querySelector('iframe')
+let trailer1 = document.querySelector('#trailer-1')
+let trailer2 = document.querySelector('#trailer-2')
+let trailer3 = document.querySelector('#trailer-3')
+
+let trailers = document.querySelectorAll('trailer')
+
+trailer1.addEventListener('click', () => {
+  trailer2.classList.remove('active');
+  trailer3.classList.remove('active');
+
+  iframe.src = 'https://www.youtube.com/embed/3KR8_igDs1Y';
+  trailer1.classList.toggle('active');
+
+})
+
+trailer2.addEventListener('click', () => {
+  trailer1.classList.remove('active');
+  trailer3.classList.remove('active');
+
+  iframe.src = 'https://www.youtube.com/embed/f3N0pzJ8w1c';
+  trailer2.classList.toggle('active');
+})
+
+trailer3.addEventListener('click', () => {
+  trailer1.classList.remove('active');
+  trailer2.classList.remove('active');
+
+  iframe.src = 'https://www.youtube.com/embed/k4xGqY5IDBE';
+  trailer3.classList.toggle('active');
+})
